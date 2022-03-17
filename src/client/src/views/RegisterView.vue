e1deac1dc8da3f0b28d28ed685d69758db928874
petar <petar.dmnv@gmail.com>
2022-03-17 12:43:11 +0200

    feat: Add new components to Register view.

--- src/client/src/views/RegisterView.vue
+++ src/client/src/views/RegisterView.vue
@@ -1,20 +1,27 @@
 <template>
     <section class="register">
         <h1> Register </h1>
-        <form>
-            <input type="username" class="form-control" id="inputUsername" placeholder="Username">
-            <input type="email" class="form-control" id="inputEmail" placeholder="Email">
-            <input type="password" class="form-control" id="inputPassword" placeholder="Password">
-            <button type="submit" class="btn btn-dark btn-lg">Register</button>
-        </form>
-        <router-link type="button" class="btn btn-light btn-lg" to="/login"> Log In </router-link>
+        <!-- Listen to 'export-data' event occurs -->
+        <UserForm @export-data="getData" :message="'Register'"></UserForm>
+        <ButtonComponent :message="'Log In'" :path="'/login'" :btnClass="'btn btn-light btn-lg'"/>
     </section>
 </template>
 
 <script>
+import UserForm from '../components/UserForm.vue';
+import ButtonComponent from '../components/ButtonComponent.vue';
 
 export default{
 
+    components:{
+        UserForm,
+        ButtonComponent
+    },
+    methods: {
+        getData(formData) {
+            console.log(formData);
+        }
+    }
 }
 </script>
 
@@ -23,35 +30,5 @@ h1 {
     color: black;
     font-weight: bold;
     font-size: xx-large;
-}
-.register {
-    display: inline-block;
-    margin-top: 7%;
-    margin-left: 35%;
-    margin-right: 35%;
-    margin-bottom: 7%;
-    width: 30%;
-    text-align: center;
-}
-.register .form-control {
-    margin-top: 7%;
-    margin-bottom: 7%;
-    background: #EEF6FB;
-    border: 1px solid #D9E4F5;
-    box-sizing: border-box;
-    border-radius: 70px;
-}
-.register .btn.btn-dark.btn-lg {
-    background-color: black;
-    border-radius: 70px;
-    width: 50%;
-    margin-bottom: 7%;
-}
-.register .btn.btn-light.btn-lg {
-    border-width: 2px;
-    border-color: black;
-    color: black;
-    border-radius: 70px;
-    width: 50%;
 }
 </style>
