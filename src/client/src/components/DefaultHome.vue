1c28379c8b39a7a9fd873ba21d5da75ccedb543d
petar <petar.dmnv@gmail.com>
2022-03-17 12:40:35 +0200

    fix: Fix page styling

--- src/client/src/components/DefaultHome.vue
+++ src/client/src/components/DefaultHome.vue
@@ -1,18 +1,20 @@
 <template>
   <section>
-      <router-link type="button" class="btn btn-light btn-lg" to="/login"> Log In </router-link>
       <h1> Platform for SAP IdM data segmentation</h1>
       <h3> Explore our site and build your own pipelines that could save you time in SAP Idm privileges management </h3>
       <h4> Hurry up!!! Join our community now! </h4>
-      <router-link type="button" class="btn btn-dark btn-lg" to="/register"> Register </router-link>
+      <div class="buttons">
+        <ButtonComponent :message="'Register'" :path="'/register'" :btnClass="'btn btn-dark btn-lg'"/>
+        <ButtonComponent :message="'Log In'" :path="'/login'" :btnClass="'btn btn-light btn-lg'"/>
+      </div>
   </section>
 </template>
 
 <script>
+import ButtonComponent from '../components/ButtonComponent.vue';
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  components: {
+    ButtonComponent
   }
 }
 </script>
@@ -34,19 +36,16 @@ li {
 a {
   color: rgb(255, 255, 255);
 }
-.btn.btn-dark.btn-lg {
+/* .btn.btn-dark.btn-lg {
   background-color: black;
   width: 10%;
   margin: 5%;
-  border-radius: 80px;
+  border-radius: 70px;
 }
 .btn.btn-light.btn-lg {
   border-color: black;
   color: black;
   border-radius: 70px;
-  margin-top: 1%;
-  margin-left: 82%;
-  margin-right: 5%;
-  width: 8%;
-}
+
+} */
 </style>
