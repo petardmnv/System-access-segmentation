7f04899e8265183ba0f7524dd64c6b07236c96b2
petar <petar.dmnv@gmail.com>
2022-03-17 12:42:52 +0200

    feat: Add new components to Log In view.

--- src/client/src/views/LogInView.vue
+++ src/client/src/views/LogInView.vue
@@ -1,21 +1,25 @@
 <template>
-    <section class="login-form">
-            <h1> Log In </h1>
-            <form>
-                <input type="credentials" class="form-control" id="inputCredentials" placeholder="Username or Email">
-                <input type="password" class="form-control" id="inputPassword" placeholder="Password">
-                <button type="submit" class="btn btn-dark btn-lg">Log In</button>
-            </form>
-            <router-link type="button" :class="'btn btn-'+ theme +' btn-lg'" to="/register"> Register </router-link>
+    <section class="login">
+        <h1> Log In </h1>
+        <!-- Listen to 'export-data' event occurs -->
+        <UserForm @export-data="getData" :message="'Log In'"></UserForm>
+        <ButtonComponent :message="'Register'" :path="'/register'" :btnClass="'btn btn-light btn-lg'"/>
     </section>
 </template>
 
 <script>
+import UserForm from '../components/UserForm.vue';
+import ButtonComponent from '../components/ButtonComponent.vue';
 
 export default{
-    data(){
-        return {
-            theme: 'light'
+
+    components:{
+        UserForm,
+        ButtonComponent
+    },
+    methods: {
+        getData(formData) {
+            console.log(formData);
         }
     }
 }
@@ -23,38 +27,9 @@ export default{
 
 <style>
 h1 {
+    padding-top: 3%;
     color: black;
     font-weight: bold;
     font-size: xx-large;
-}
-.login-form {
-    display: inline-block;
-    margin-top: 7%;
-    margin-left: 35%;
-    margin-right: 35%;
-    margin-bottom: 7%;
-    width: 30%;
-    text-align: center;
-}
-.login-form .form-control {
-    margin-top: 7%;
-    margin-bottom: 7%;
-    background: #EEF6FB;
-    border: 1px solid #D9E4F5;
-    box-sizing: border-box;
-    border-radius: 70px;
-}
-.login-form .btn.btn-dark.btn-lg {
-    background-color: black;
-    border-radius: 70px;
-    width: 50%;
-    margin-bottom: 7%;
-}
-.login-form .btn.btn-light.btn-lg {
-    border-width: 2px;
-    border-color: black;
-    color: black;
-    border-radius: 70px;
-    width: 50%;
 }
 </style>
