04b788011b9e78a365ef3fc06eb18e04a7b8c803
petar <petar.dmnv@gmail.com>
2022-03-17 12:41:12 +0200

    feat: Add new button component.

---
+++ src/client/src/components/ButtonComponent.vue
@@ -0,0 +1,47 @@
+<template>
+    <router-link type="button" :class="btnClass" :to="path"> {{ message }} </router-link>
+</template>
+
+<script>
+export default {
+    props: {
+        message: {
+            type: String,
+            requred: true,
+            default: "Log In"
+        },
+        path: {
+            type: String,
+            requred: true,
+            default: "/login"
+        },
+        btnClass: {
+            type: String,
+            requred: true,
+            default: "btn btn-light btn-lg"
+        }
+    }
+}
+</script>
+
+<style scoped>
+.btn.btn-light.btn-lg {
+    border-width: 2px;
+    border-color: black;
+    color: black;
+    border-radius: 70px;
+    margin-left: 35%;
+    margin-right: 35%;
+    width: 15%;
+    text-align: center;
+
+}
+.btn.btn-dark.btn-lg {
+    background-color: black;
+    border-radius: 70px;
+    margin-left: 35%;
+    margin-right: 35%;
+    width: 15%;
+    text-align: center;
+}
+</style>
