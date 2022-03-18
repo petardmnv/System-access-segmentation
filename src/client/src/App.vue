999e58bf701dac661f37928b50b00acddff144ad
petar <petar.dmnv@gmail.com>
2022-03-18 17:53:59 +0200

    fix: Fix template error

--- src/client/src/App.vue
+++ src/client/src/App.vue
@@ -1,5 +1,6 @@
 <template>
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+<section>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
   <nav class="nav justify-content-center">
     <router-link class="nav-link" to="/home">Home</router-link>
     <router-link class="nav-link" to="/pipelines">Pipelines</router-link>
@@ -7,6 +8,7 @@
     <router-link class="nav-link" to="/models">Models</router-link>
   </nav>
   <router-view></router-view>
+</section>
 </template>
 
 <style>
