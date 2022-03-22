f74f54e36eb91dde6896bfa2a4645319a85796ae
petar <petar.dmnv@gmail.com>
2022-03-22 02:42:56 +0200

    feat: Add require field for all properties in user shema.

--- src/server/src/models/user.js
+++ src/server/src/models/user.js
@@ -4,9 +4,18 @@ import mongoose from "mongoose";
 const User = mongoose.model(
     "User",
     new mongoose.Schema({
-      username: String,
-      email: String,
-      password: String
+        username: {
+            type: String,
+            required: true
+        },
+        email: {
+            type: String,
+            required: true
+        },
+        password: {
+            type: String,
+            required: true
+        }
     })
-  );
-  module.exports = User;
+);
+module.exports = User;
