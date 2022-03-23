173c337b5c5e23a2f5eae1f57c8eec3660650dae
petar <petar.dmnv@gmail.com>
2022-03-23 22:04:36 +0200

    feat: Add token generation and error handling for that token to register function.

--- src/server/src/controllers/auth/auth.js
+++ src/server/src/controllers/auth/auth.js
@@ -12,7 +12,16 @@ module.exports = {
                 password: password
             });
             if (!user) res.status(400).send({ message: "Unable to register." });
-            res.send({ message: "User registered successfully!" });
+
+            // Remove user from db if jwt token generation fails
+            let token = {};
+            try {
+                token = await user.generateAuthToken();
+            } catch (error) {
+                await User.deleteOne({ username });
+                res.status(400).send({ message: error.message });
+            }
+            res.status(200).send({ token: token, user: user });
         } catch (error) {
             res.status(500).send({ message: error.message });
         }
