2f97944c05b935bea23dbf8a1f16b949c0fcf3b1
petar <petar.dmnv@gmail.com>
2022-03-23 14:58:48 +0200

    feat: Add post request handler on uri /login or simply add login logic.

--- src/server/src/routes/auth/auth.js
+++ src/server/src/routes/auth/auth.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../../models/user.js');
 
-router.get('/register', (req, res) => {
-    
+router.post('/login', async (req, res) => {
+    let {username, email, password} = req.body;
+    console.log(username);
+    try {
+        const user = await User.findByCredentials(username, email, password);
+        console.log(`User props: ${user}`);
+        res.send(user).status(200);
+    } catch (error) {
+        console.log("Fuck")
+        console.log(error.message)
+        res.status(401).send({ message: error.message })
+    }
 });
 
 module.exports = router;
