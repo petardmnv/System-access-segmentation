7647db9c5883296d05c8b3fa3e5faaec1da26a22
petar <petar.dmnv@gmail.com>
2022-03-23 14:55:36 +0200

    feat: Add static method for comparing password and hash password method that runs on every save event in the collection.

--- src/server/src/models/user.js
+++ src/server/src/models/user.js
@@ -1,21 +1,72 @@
-import mongoose from "mongoose";
+const mongoose = require("mongoose");
+const validator = require('validator');
+const bcrypt = require('bcrypt');
 
 // Create model which has its data schema for User information in the site
-const User = mongoose.model(
-    "User",
-    new mongoose.Schema({
-        username: {
-            type: String,
-            required: true
-        },
-        email: {
-            type: String,
-            required: true
-        },
-        password: {
-            type: String,
-            required: true
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Email is invalid');
+            }
         }
-    })
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 8
+    }
+});
+
+userSchema.statics.findByCredentials = async function(username, email, password){
+    let userData = null;
+    if (!username) {
+        userData = await User.findOne({ email });
+    }
+    else {
+        userData = await User.findOne({ username });
+    }
+
+    // check if data is empty 
+    if (!userData) {
+        throw new Error("Unable to login.");
+    }
+
+    // compare passwwords
+    let isValid = await bcrypt.compare(password, userData.password);
+
+    if (!isValid) {
+        throw new Error("Unable to login.");
+    }
+    // if everything passes return data for the current user
+    return userData;
+}
+
+// Add middleware to the data collection - like hashing password method
+// We need to hash the password before an event like save() occurs
+// so I will use build in pre() function that will execute this method before a save event occurs
+userSchema.pre('save', async function(next) {
+    //this is reference to teh documenta that we are modifying via save event
+    // isModified(attr) is used to check if the password is modified. If it's not then i don't need
+    // to hash it again
+    console.log(this.password);
+    if (this.isModified('password')) {
+        this.password = await bcrypt.hash(this.password, 8);
+    }
+    console.log(this.password);
+    next();
+});
+
+const User = mongoose.model(
+    "User", userSchema
 );
 module.exports = User;
