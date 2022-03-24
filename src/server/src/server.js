beb23b93ecd229dd1a76b86fb91f40fed930cf29
petar <petar.dmnv@gmail.com>
2022-03-24 14:53:47 +0200

    fix: Add functionalities for catching unhandled synchronous and asynchronous exceptions.

--- src/server/src/server.js
+++ src/server/src/server.js
@@ -1,5 +1,13 @@
 const { port } = require('./config/index.js');
 const mongoose = require("mongoose");
+
+// Catch synchronous exeptions 
+process.on('uncaughtException', err => {
+  console.log("Uncaught Exception. Startng shut down ...");
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 const app = require('./app.js');
 const { createConnection } = require('./models/connect.js');
 createConnection(mongoose);
@@ -11,6 +19,17 @@ mongoose.connection.on("error", function (error) {
 
 // set port who listens for requests
 const PORT = port || 8081;
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
+});
+
+// Add global unhandled error event listener because it is posssible 
+// to have uncaught errors/promise rejections somewhere in my code
+process.on('unhandledRejection', err => {
+  console.log("Unhandled Rejection. Startng shut down ...");
+  console.log(err.name, err.message);
+  // shut down server but wait for pending requests to finish
+  server.close(() => {
+    process.exit(1);
+  });
 });
