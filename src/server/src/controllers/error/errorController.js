8757dc3e29924dff0b1e552904ce6bb72035aefc
petar <petar.dmnv@gmail.com>
2022-03-24 10:31:44 +0200

    feat: Add middleware for response error handling.

---
+++ src/server/src/controllers/error/errorController.js
@@ -0,0 +1,11 @@
+module.exports = {
+    globalErrorHandler: (err, req, res, next) => {
+        // Set resp status code from err.statusCode or set default = 500
+        err.statusCode = err.statusCode || 500;
+        
+        //send response with proveded  status and message
+        res.status(err.statusCode).send({
+          message: err.message
+        });
+    }
+};
