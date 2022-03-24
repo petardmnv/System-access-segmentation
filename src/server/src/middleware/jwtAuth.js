f0976e61652b10da5d442da317fd6cc0faf0c476
petar <petar.dmnv@gmail.com>
2022-03-24 14:58:42 +0200

    feat: Add middleware functionality for token authorization and route protection. From now on if route is protected just the owner of a valid token can get acces to route resourse.

--- src/server/src/middleware/jwtAuth.js
+++ src/server/src/middleware/jwtAuth.js
@@ -1,7 +1,10 @@
+const jwt = require("jsonwebtoken");
 const AppError = require("../utils/appError");
+const { secret } = require('../config/auth.js');
+const User = require('../models/user.js')
 
-modeule.exports = {
-    protectRoute: async (req, _, next) => {
+module.exports = {
+    protectRoute: async (req, res, next) => {
         // Get the auth ktoken from Authorization Header from the request
         // Structure Authorization: Bearer <JWT token>
         let token;
@@ -11,5 +14,30 @@ modeule.exports = {
         if (!token) {
             return next(new AppError("Unable to get authorization token", 401));
         }
+        // Verify token 
+        let decodedToken;
+        try {
+            decodedToken = await jwt.verify(token, secret);
+        } catch (error) {
+            next(new AppError(error.message, 401));
+        }
+
+        // Ckeck if user who has his id writen into the token exists
+        const currentUser = await User.findById(decodedToken.id);
+        if (!currentUser) {
+            return next(new AppError("User who belongs to this token no longer exists.", 401));
+        }
+
+        // check if user changed his pass after token was generated
+        if (currentUser.changedPasswordAfterTokenGeneration(decodedToken.iat)){
+            return next(new AppError("User who belongs to this token has changed his password after generation of the token.", 401));
+        }
+
+        // If all checks passed then give current user data to the req and call next()
+        console.log(currentUser);
+        let currentUserData = {id: currentUser._id, username: currentUser.username, email: currentUser.email }
+
+        req.user = currentUserData;
+        next();
     }
 };
