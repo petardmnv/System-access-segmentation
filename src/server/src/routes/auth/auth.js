36c2d83326edb41313157264e63c1a73f804cc74
petar <petar.dmnv@gmail.com>
2022-03-24 14:54:39 +0200

    feat: Add new route for testing protectRoute middleware.

--- src/server/src/routes/auth/auth.js
+++ src/server/src/routes/auth/auth.js
@@ -2,9 +2,14 @@ const express = require('express');
 const router = express.Router();
 const User = require('../../models/user.js');
 const { register, login } = require('../../controllers/auth/authController.js');
+const { protectRoute } = require('../../middleware/jwtAuth.js')
 
 router.post('/login', login);
 
 router.post('/register', register);
+
+router.get('/pipeline', protectRoute, (req, res) => {
+    res.status(200).send(req.user);
+});
 
 module.exports = router;
