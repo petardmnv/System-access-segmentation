7bded81beaaceaba6ad28879af3c354d2601830e
petar <petar.dmnv@gmail.com>
2022-03-24 10:37:24 +0200

    fix: Add AppError functionality to functions and refactor some of the code.

--- src/server/src/models/user.js
+++ src/server/src/models/user.js
@@ -4,6 +4,7 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const { saltRounds } = require('../config/salt.js');
 const { secret } = require('../config/auth.js');
+const AppError = require('../utils/appError.js')
 
 // Create model which has its data schema for User information in the site
 const userSchema = new mongoose.Schema({
@@ -37,14 +38,14 @@ const userSchema = new mongoose.Schema({
 });
 
 // Instanse available function for creating authorization token - JWT
-userSchema.methods.generateAuthToken = async function() {
+userSchema.methods.generateAuthToken = async function () {
     // generate token. Get user id from mongo collection and add it to the token payload
     const token = await jwt.sign({ id: this._id.toString() }, secret, { expiresIn: "2 days" });
     return token;
 }
 
 // Model available function for authenticationg user by given credentials
-userSchema.statics.findByCredentials = async function(username, email, password){
+userSchema.statics.findByCredentials = async function (username, email) {
     // Get user by providing username or email
     let userData = null;
     if (!username) {
@@ -56,9 +57,9 @@ userSchema.statics.findByCredentials = async function(username, email, password)
 
     // check if data is empty 
     if (!userData) {
-        throw new Error("Unable to login.");
+        throw new AppError("Unable to login.", 401);
     }
-    
+
     // if everything passes return data for the current user
     return userData;
 }
@@ -66,8 +67,8 @@ userSchema.statics.findByCredentials = async function(username, email, password)
 // Add middleware to the data collection - like hashing password method
 // We need to hash the password before an event like save() occurs
 // so I will use build in pre() function that will execute this method before a save event occurs
-userSchema.pre('save', async function(next) {
-    //this is reference to teh documenta that we are modifying via save event
+userSchema.pre('save', async function (next) {
+    //this is reference to the documenta that we are modifying via save event
     // isModified(attr) is used to check if the password is modified. If it's not then i don't need
     // to hash it again
     if (this.isModified('password')) {
