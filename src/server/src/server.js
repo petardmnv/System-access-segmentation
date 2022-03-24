a039478a81021518eb41de56f06329850ff48787
petar <petar.dmnv@gmail.com>
2022-03-24 10:34:54 +0200

    feat: Add error middleware and error hendler for invalid paths

--- src/server/src/server.js
+++ src/server/src/server.js
@@ -3,6 +3,9 @@ const express = require('express');
 const mongoose = require("mongoose");
 const app = express();
 
+const { globalErrorHandler } = require('./controllers/error/errorController.js');
+const AppError = require('./utils/appError.js');
+
 const { createConnection } = require('./models/connect.js');
 createConnection(mongoose);
 
@@ -16,6 +19,16 @@ const authRouter = require('./routes/auth/auth.js');
 app.use(express.json());
 
 app.use('/', authRouter);
+
+// Handler for all invalid server urls
+app.all('*', (req, res, next) => {
+  // pass the error to next(). If an error occurs in the next parameter all other middleware func are
+  // skipped and the error is handled by the global middleware error handler 
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+// Error handling middleware
+app.use(globalErrorHandler);
 
 // set port who listens for requests
 const PORT = port || 8081;
