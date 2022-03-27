f163251156f29f254369fe91a4dfc6342537ec3a
petar <petar.dmnv@gmail.com>
2022-03-27 04:07:18 +0300

    feat: Add integration of the component with vue auth storage. Functionality for extracting data from UserFrm and passing it to auth store then store passes response object back. Logic for displaing to user server errors.

--- src/client/src/views/LogInView.vue
+++ src/client/src/views/LogInView.vue
@@ -1,36 +1,59 @@
 <template>
-    <section class="login">
-        <h1> Log In </h1>
-        <!-- Listen to 'export-data' event occurs -->
-        <UserForm @export-data="getData" :message="'Log In'"></UserForm>
-        <ButtonComponent :message="'Register'" :path="'/register'" :btnClass="'btn btn-light btn-lg'"/>
-    </section>
+  <section class="login">
+      <InfoDialog :show="!!error" :heading="error" @close="closeDialog"/>
+    <h1>Log In</h1>
+    <!-- Listen to 'export-data' event occurs -->
+    <UserForm @export-data="getData" :message="'Log In'"></UserForm>
+    <ButtonComponent
+      :message="'Register'"
+      :path="'/register'"
+      :btnClass="'btn btn-light btn-lg'"
+    />
+  </section>
 </template>
 
 <script>
-import UserForm from '../components/UserForm.vue';
-import ButtonComponent from '../components/ButtonComponent.vue';
+import store from "@/store";
+import UserForm from "../components/UserForm.vue";
+import ButtonComponent from "../components/ButtonComponent.vue";
+import InfoDialog from "@/ui/InfoDialog.vue";
 
 export default {
-    components: {
-        UserForm,
-        ButtonComponent
+  data() {
+    return {
+      error: null,
+    };
+  },
+  components: {
+    UserForm,
+    ButtonComponent,
+    InfoDialog
+  },
+  methods: {
+    getData(formData) {
+      this.sendRequest(formData);
     },
-    methods: {
-        getData(formData) {
-            console.log(formData);
-        }
+    async sendRequest(reqData) {
+      try {
+        await store.dispatch("login", reqData);
+        // Redirect
+        this.$router.replace("/home");
+      } catch (error) {
+        this.error = error.message || "Failed to Login. Try again.";
+      }
+    },
+    closeDialog(){
+      this.error = null;
     }
-    //Send https request
-    
+  }
 };
 </script>
 
 <style>
 h1 {
-    padding-top: 30px;
-    color: black;
-    font-weight: bold;
-    font-size: xx-large;
+  padding-top: 30px;
+  color: black;
+  font-weight: bold;
+  font-size: xx-large;
 }
 </style>
