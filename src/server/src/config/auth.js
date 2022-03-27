f622c6d0fef06f5feb82fe982f7021d438e1d25d
petar <petar.dmnv@gmail.com>
2022-03-28 01:40:06 +0300

    feat: Parsed cookie expiration to integer.

--- src/server/src/config/auth.js
+++ src/server/src/config/auth.js
@@ -3,6 +3,6 @@ dotenv.config({path: './.env'});
 module.exports = {
     secret: process.env.JWT_SECRET,
     tokenExpiration: process.env.TOKEN_EXPIRATION,
-    cookieExpiration: process.env.COOKIE_EXPIRATION,
+    cookieExpiration: parseInt(process.env.COOKIE_EXPIRATION, 10),
     nodeEnv: process.env.NODE_ENV
 };
