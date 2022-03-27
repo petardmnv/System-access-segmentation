eefe931b7251abecad6dc45ea1816b0c21092e69
petar <petar.dmnv@gmail.com>
2022-03-27 04:09:46 +0300

    feat: Add functionality for loged user UI and unloged user UI.

--- src/client/src/App.vue
+++ src/client/src/App.vue
@@ -1,15 +1,30 @@
 <template>
-<section>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-  <nav class="nav justify-content-center">
-    <router-link class="nav-link" to="/home">Home</router-link>
-    <router-link class="nav-link" to="/pipelines">Pipelines</router-link>
-    <router-link class="nav-link" to="/data">Data</router-link>
-    <router-link class="nav-link" to="/models">Models</router-link>
-  </nav>
-  <router-view></router-view>
-</section>
+  <section>
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
+      crossorigin="anonymous"
+    />
+    <nav v-if="isAuthenticated" class="nav justify-content-center">
+      <router-link class="nav-link" to="/home">Home</router-link>
+      <router-link class="nav-link" to="/pipelines">Pipelines</router-link>
+      <router-link class="nav-link" to="/data">Data</router-link>
+      <router-link class="nav-link" to="/models">Models</router-link>
+    </nav>
+    <router-view></router-view>
+  </section>
 </template>
+
+<script>
+export default {
+  computed: {
+    isAuthenticated() {
+      return this.$store.getters.isAuthenticated;
+    },
+  },
+};
+</script>
 
 <style>
 #app {
@@ -26,5 +41,7 @@ nav.nav.justify-content-center a {
 nav.nav.justify-content-center a.active.router-link-exact-active.nav-link {
   text-decoration-thickness: 4px;
   text-decoration-line: underline;
+  display: flex;
+  justify-content: center;
 }
 </style>
