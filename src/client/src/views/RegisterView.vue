adc3b5052c77830ea3a980884e4903cd07b08a53
petar <petar.dmnv@gmail.com>
2022-03-27 04:08:45 +0300

    feat: Add Registration integration of the component with vue auth storage. Functionality for extracting data from UserForm and passing it to auth store then store passes response object back. Logic for displaing to user server errors.

--- src/client/src/views/RegisterView.vue
+++ src/client/src/views/RegisterView.vue
@@ -1,36 +1,58 @@
 <template>
-    <section class="register">
-        <h1> Register </h1>
-        <!-- Listen to 'export-data' event occurs -->
-        <UserForm @export-data="getData" :message="'Register'"></UserForm>
-        <ButtonComponent :message="'Log In'" :path="'/login'" :btnClass="'btn btn-light btn-lg'"/>
-    </section>
+  <section class="register">
+    <InfoDialog :show="!!error" :heading="error" @close="closeDialog"/>
+    <h1>Register</h1>
+    <!-- Listen to 'export-data' event occurs -->
+    <UserForm @export-data="getData" :message="'Register'"></UserForm>
+    <ButtonComponent
+      :message="'Log In'"
+      :path="'/login'"
+      :btnClass="'btn btn-light btn-lg'"
+    />
+  </section>
 </template>
 
 <script>
-import UserForm from '../components/UserForm.vue';
-import ButtonComponent from '../components/ButtonComponent.vue';
+import UserForm from "../components/UserForm.vue";
+import ButtonComponent from "../components/ButtonComponent.vue";
+import store from "@/store";
+import InfoDialog from "@/ui/InfoDialog.vue";
 
 export default {
-
-    components:{
-        UserForm,
-        ButtonComponent
+  data() {
+    return {
+      error: null
+    };
+  },
+  components: {
+    UserForm,
+    ButtonComponent,
+    InfoDialog
+  },
+  methods: {
+    getData(formData) {
+      this.sendRequest(formData);
     },
-    methods: {
-        getData(formData) {
-            console.log(formData);
-        }
+    async sendRequest(reqData) {
+      try {
+        await store.dispatch("register", reqData);
+        // Redirect
+        this.$router.replace("/home");
+      } catch (error) {
+        this.error = error.message || "Failed to Register. Try again.";
+      }
+    },
+    closeDialog(){
+      this.error = null;
     }
-    // Send https request
-    
+  },
 };
 </script>
 
 <style>
 h1 {
-    color: black;
-    font-weight: bold;
-    font-size: xx-large;
+  color: black;
+  font-weight: bold;
+  font-size: xx-large;
 }
 </style>
