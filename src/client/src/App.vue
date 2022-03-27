b22333780ed6a8aa1bc827d6d7d5e3433489a5d9
petar <petar.dmnv@gmail.com>
2022-03-28 01:33:08 +0300

    fix: Clear code in app.vue

--- src/client/src/App.vue
+++ src/client/src/App.vue
@@ -1,28 +1,20 @@
 <template>
-  <section>
+  <div>
     <link
       href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
       rel="stylesheet"
       integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
       crossorigin="anonymous"
     />
-    <nav v-if="isAuthenticated" class="nav justify-content-center">
-      <router-link class="nav-link" to="/home">Home</router-link>
-      <router-link class="nav-link" to="/pipelines">Pipelines</router-link>
-      <router-link class="nav-link" to="/data">Data</router-link>
-      <router-link class="nav-link" to="/models">Models</router-link>
-    </nav>
+    <NavBar/>
     <router-view></router-view>
-  </section>
+  </div>
 </template>
 
 <script>
+import NavBar from "./components/NavBar.vue";
 export default {
-  computed: {
-    isAuthenticated() {
-      return this.$store.getters.isAuthenticated;
-    },
-  },
+    components: { NavBar }
 };
 </script>
 
@@ -32,16 +24,5 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-}
-nav.nav.justify-content-center a {
-  font-weight: bold;
-  font-size: larger;
-  color: black;
-}
-nav.nav.justify-content-center a.active.router-link-exact-active.nav-link {
-  text-decoration-thickness: 4px;
-  text-decoration-line: underline;
-  display: flex;
-  justify-content: center;
 }
 </style>
