c5984b873b4a981d553500edd2deba203fc07270
petar <petar.dmnv@gmail.com>
2022-03-28 01:43:44 +0300

    feat: Add main nav bar into separate component for better code readability.

---
+++ src/client/src/components/NavBar.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <nav class="nav justify-content-center">
+      <router-link class="nav-link" to="/home">Home</router-link>
+      <router-link class="nav-link" to="/pipelines">Pipelines</router-link>
+      <router-link class="nav-link" to="/data">Data</router-link>
+      <router-link class="nav-link" to="/models">Models</router-link>
+    </nav>
+  </div>
+</template>
+
+<style>
+nav.nav.justify-content-center a {
+  font-weight: bold;
+  font-size: larger;
+  color: black;
+}
+nav.nav.justify-content-center a.active.router-link-exact-active.nav-link {
+  text-decoration-thickness: 4px;
+  text-decoration-line: underline;
+  display: flex;
+  justify-content: center;
+}
+</style>
