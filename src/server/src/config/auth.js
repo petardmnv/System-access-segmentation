876d272010d904f0bd5d22730bbce6c8f23af147
petar <petar.dmnv@gmail.com>
2022-03-27 04:13:53 +0300

    fix: Remove CLIENT_PORT environment varable from auth.js.

--- src/server/src/config/auth.js
+++ src/server/src/config/auth.js
@@ -1,8 +1,8 @@
 const dotenv = require('dotenv');
 dotenv.config({path: './.env'});
 module.exports = {
-    secret: process.env.JWTSECRET,
-    tokenExpiration: process.env.TOKENEXPIRATION,
-    cookieExpiration: process.env.COOKIEEXPIRATION,
+    secret: process.env.JWT_SECRET,
+    tokenExpiration: process.env.TOKEN_EXPIRATION,
+    cookieExpiration: process.env.COOKIE_EXPIRATION,
     nodeEnv: process.env.NODE_ENV
 };
