d1b2104d392bd13ded31e5f975f6eb2b9facddfd
petar <petar.dmnv@gmail.com>
2022-03-29 00:49:11 +0300

    feat: Add new Component component which acts like content style wrapper.

---
+++ src/client/src/components/ContentComponent.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="contentContainer">
+    <slot></slot>
+  </div>
+</template>
+
+<style scoped>
+.contentContainer {
+  border-radius: 12px;
+  border: 2px solid black;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  padding: 2rem;
+  margin: 2rem auto;
+  max-width: 50rem;
+  max-height: 30rem;
+  text-align: center;
+}
+</style>
