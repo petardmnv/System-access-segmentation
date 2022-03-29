fd906ca150ba36041f3d558c91fb1464fcd1e182
petar <petar.dmnv@gmail.com>
2022-03-29 15:34:50 +0300

    feat: Add loading spiner component.

---
+++ src/client/src/components/SpinnerComponent.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="spinner" v-if="isLoading">
+    <ContentComponent class="new">
+      <div
+        class="spinner-border"
+        style="width: 3rem; height: 3rem"
+        role="status">
+        <span class="visually-hidden">Loading...</span>
+      </div>
+    </ContentComponent>
+  </div>
+</template>
+
+<script>
+import ContentComponent from "./ContentComponent.vue";
+export default {
+  props: {
+    isLoading: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  components: { ContentComponent },
+};
+</script>
+
+<style scoped>
+.new{
+    max-width: 20rem;
+    max-height: 20rem;
+    align-items: center;
+    vertical-align: middle;
+}
+</style>
