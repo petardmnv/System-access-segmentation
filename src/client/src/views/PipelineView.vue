2899312c18b3c7402481c4f10088ff8b4a0b1e71
petar <petar.dmnv@gmail.com>
2022-03-29 15:32:16 +0300

    feat: Add page outlook.

--- src/client/src/views/PipelineView.vue
+++ src/client/src/views/PipelineView.vue
@@ -1,9 +1,41 @@
 <template>
-  <h1>Pipelines</h1>
+  <div>
+    <h1>Run Pipeline</h1>
+    <content-component>
+      <h3>
+        Import needed data to execute pipeline
+      </h3>
+      <pipeline-component>
+
+      </pipeline-component>
+    </content-component>
+    <SpinnerComponent :isLoading="isLoading"></SpinnerComponent>
+  </div>
 </template>
 
 <script>
-import ButtonComponent from "../components/ButtonComponent.vue";
-
-export default {};
+import SpinnerComponent from "@/components/SpinnerComponent.vue";
+export default { 
+  components: { SpinnerComponent },
+  data() {
+    return {
+      isLoading: false,
+      formData: null
+    }
+  },
+  computed: {
+    isLoading() {
+      return this.isLoading;
+    }
+  }  
+};
 </script>
+
+<style scoped>
+h1 h3 {
+  font-weight: 900;
+  font-size: 24px;
+  line-height: 30px;
+  margin-bottom: 5rem;
+}
+</style>
