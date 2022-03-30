09c5634071ac22165aacfd3e9173481c78b9497e
petar <petar.dmnv@gmail.com>
2022-03-30 05:35:55 +0300

    feat: Add controllers for saving result, fetching all results for current user.

--- src/server/src/controllers/pipeline/pipelineController.js
+++ src/server/src/controllers/pipeline/pipelineController.js
@@ -14,7 +14,9 @@ const storage = multer.diskStorage({
     filename: function (req, file, cb) {
         // get file extention
         const ext = file.originalname.split('.')[file.originalname.split('.').length - 1];
-        cb(null, `User-${req.user.id}-${Date.now()}.${ext}`);
+        const newFileName = `User-${req.user.id}-${Date.now()}.${ext}`;
+        req.body.filename = newFileName;
+        cb(null, newFileName);
     }
 });
 
@@ -37,5 +39,48 @@ const upload = multer({
 
 
 module.exports = {
-    uploadFile: upload.single('file')
+    uploadFile: upload.single('file'),
+    runPipeline(req, res) {
+        // Provide data to model functions
+        let { department, job, filename } = req.body;
+        // runModel(filename, departmen, job);
+        // Simulate model result 
+        let result = ['QLOOR1', 'RST23', '23RTY4', 'QTRL721', 'RTWE34', 'RS6', 'ASDF', 'PRIV1', 'PRIV2', 'PRIV3'];
+        setTimeout(() => {
+            res.status(200).send({ privileges: result });
+        }, 1 * 1000);
+    },
+    async saveResult(req, res, next) {
+        try {
+            // get params from request body and check if they are null
+            let {role, privileges} = req.body
+            let userId = req.user.id;
+            if (!role || !privileges) {
+                return next(new AppError("Role or privileges is empty", 400));
+            }
+            const result = await Result.create({
+                role: role,
+                privileges: privileges,
+                userId: userId
+            });
+
+            if (!result) {
+                return next(new AppError("Error while caving result.", 500));
+            }
+            res.sendStatus(200);
+        } catch (error) {
+            return next(new AppError(error.message, 500));
+        }
+    },
+    async getResults(req, res, next) {
+        try {
+            let results = await Result.find({userId: req.user.id});
+            if (!results) {
+                return next(new AppError('There are no results for this user.', 404));
+            }
+            res.status(200).send({results: results});
+        } catch (error) {
+            return next(new AppError(error.message, 500));
+        }
+    }
 };
