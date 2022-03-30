3b09cb4af09e372d8c3d4ef5c1a288d5a73fa3a0
petar <petar.dmnv@gmail.com>
2022-03-31 01:13:49 +0300

    feat: Add NotFound page.

---
+++ src/client/src/views/NotFound.vue
@@ -0,0 +1,8 @@
+<template>
+<badge-component>
+    <div class="about">
+    <h1>This page is not found</h1>
+    <p>Maybe go to <router-link to="/home">Home</router-link></p>
+  </div>
+</badge-component>
+</template>
