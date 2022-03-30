a60c5ba7f62da9db541b2cf974d9bf67f2450553
petar <petar.dmnv@gmail.com>
2022-03-30 05:35:03 +0300

    feat: Add passing data to the server functionaliti and waiting for repose with error handling.

--- src/client/src/views/PipelineView.vue
+++ src/client/src/views/PipelineView.vue
@@ -1,15 +1,14 @@
 <template>
   <div>
+    <dialog-component :show="!!error" :heading="error" @close="closeDialog" />
     <h1>Run Pipeline</h1>
-    <content-component>
+    <card-component>
       <h3>
         Import needed data to execute pipeline
       </h3>
-      <pipeline-component>
-
-      </pipeline-component>
-    </content-component>
-    <SpinnerComponent :isLoading="isLoading"></SpinnerComponent>
+      <pipeline-component @export-pipeline-data="getData"/>
+    </card-component>
+    <SpinnerComponent :isLoading="isLoading" class="loading"></SpinnerComponent>
   </div>
 </template>
 
@@ -19,15 +18,31 @@ export default {
   components: { SpinnerComponent },
   data() {
     return {
-      isLoading: false,
-      formData: null
+      formData: null,
+      error: null,
+      isLoading: false
     }
   },
-  computed: {
-    isLoading() {
-      return this.isLoading;
-    }
-  }  
+  methods: {
+    getData(formData) {
+      this.sendRequest(formData);
+    },
+    async sendRequest(reqData) {
+      try {
+        this.isLoading = true;
+        await this.$store.dispatch("results/sendPipelineData", reqData);
+        this.isLoading = false;
+        // Redirect
+        this.$router.replace("/pipelines/result");
+      } catch (error) {
+        this.error = error.message || "Failed to send Pipeline data to the server.";
+        this.isLoading = false;
+      }
+    },
+    closeDialog() {
+      this.error = null;
+    },
+  }
 };
 </script>
 
@@ -37,5 +52,10 @@ h1 h3 {
   font-size: 24px;
   line-height: 30px;
   margin-bottom: 5rem;
+}
+.loading {
+  position: absolute;
+  height: 100vh;
+  width: 100vw;
 }
 </style>
