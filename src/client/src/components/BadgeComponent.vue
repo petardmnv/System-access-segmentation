6193ae0b1466b47f4ef8a85709ec1b53f1bc96bb
petar <petar.dmnv@gmail.com>
2022-03-30 05:38:06 +0300

    feat: Add temporary badge component. It is not finished yet.

---
+++ src/client/src/components/BadgeComponent.vue
@@ -0,0 +1,34 @@
+<template>
+  <span class="badge" :class="type">
+    {{ text }}
+  </span>
+</template>
+
+<script>
+export default {
+  props: ["type", "title"],
+  computed: {
+    text() {
+      return this.title;
+    },
+  },
+};
+</script>
+
+<style scoped>
+span {
+    font-size: 700;
+}
+span .privileges {
+  background-color: #eef6fb;
+  color: rgb(0, 0, 0);
+}
+.badge {
+  background-color: #eef6fb;
+  color: #252525;
+  border-radius: 30px;
+  padding: 0.5rem 1.5rem;
+  display: inline-block;
+  margin-right: 0.5rem;
+}
+</style>
